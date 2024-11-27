e7ab77eebc0bf795d9ba6a1b18792a88eceb37b8
Rupendra <rupendra@example.com>
2024-11-27 12:09:23 +0700

    ATProgressOverlayModifier implemented.

---
+++ Sources/ATKit/ViewsAndControls/ATProgressOverlayModifier.swift
@@ -0,0 +1,40 @@
+//
+//  ATProgressOverlayModifier.swift
+//  ATKit
+//
+//  Created by Rupendra on 27/11/24.
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+
+@available(iOS 14.0, *)
+struct ATProgressOverlayModifier: ViewModifier {
+    var isTaskInProgress: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                Group {
+                    if self.isTaskInProgress {
+                        Color.black
+                            .opacity(0.4)
+                            .ignoresSafeArea()
+                        
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle(tint: Color.white))
+                            .scaleEffect(2.0)
+                            .background(Color.clear)
+                    }
+                }
+            )
+    }
+}
+
+@available(iOS 14.0, *)
+extension View {
+    func progressOverlay(isTaskInProgress pIsTaskInProgress: Bool) -> some View {
+        self.modifier(ATProgressOverlayModifier(isTaskInProgress: pIsTaskInProgress))
+    }
+}
